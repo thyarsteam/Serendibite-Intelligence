aa87e5cbafa7d4b2c114528665da3e190383bc04
Anirudh Rajesh <thyarsteam@gmail.com>
2026-05-16 20:50:01 +0530

    Serendibite Intelligence by Anirudh Rajesh

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "serendibite.firebaseapp.com",
-  projectId: "serendibite",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "1061642988257",
-  appId: "1:1061642988257:web:4379659973fecb27c165a0",
-  measurementId: "G-1B57M1PS3N"
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
